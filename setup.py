f4fcd9a646f4c230d3b4b7a87997555c0e0a72ec
amnona <amnonim@gmail.com>
2017-03-16 16:35:13 -0700

    fix setup.py

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(name='ezcalour',
       url='http://microbio.me/calour',
       test_suite='nose.collector',
       packages=find_packages(),
-      package_data={'calour': ['log.cfg']},
+      package_data={'ezcalour': ['ui/*']},
       scripts=['ezcalour.py'],
       install_requires=[
           'calour'],
